b51e0131520befd46f0d2bbdad543b4bd013a3df
banosarwari8928 <banosarwari8928@gmail.com>
2025-12-09 11:33:28 +0430

    PATCH method worked in dynamic route handler for updating

--- src/app/posts/[id]/route.ts
+++ src/app/posts/[id]/route.ts
@@ -4,3 +4,12 @@ export async function  GET(request:Request,{params}:{params:Promise<{id:string}>
  const index=data.findIndex((post)=>post.id===parseInt(id));
  return Response.json(data[index]);
 }
+export async function PATCH(request:Request,{params}:{params:Promise<{id:string}>}){
+    const {id}=await params;
+    const dataU =await request.json();
+    const{title,content}=dataU;
+    const index=data.findIndex((post)=>post.id===parseInt(id));
+    data[index].title;
+    data[index].content;
+    return Response.json(data[index]);
+}
