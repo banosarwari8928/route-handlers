ff8ac47f7edfd504b348b186d668ba94bdd7c22e
banosarwari8928 <banosarwari8928@gmail.com>
2025-12-16 09:55:47 +0430

    some canges added

--- src/app/RouteHandler/Dashboard/user/Api/route.ts
+++ src/app/RouteHandler/Dashboard/user/Api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-    return new Response("hello from API");
+import {type NextRequest } from "next/server";
+import { headers } from "next/headers";
+export async function GET(request:NextRequest) {
+    const header = new Headers(request.headers);
+    console.log(header.get('Authorization'))
+    return new Response("hello from API",{
+    headers: {
+        "Content-Type":"text/html",
+        "Set-Cookie":"theme/light"
+    }
+});
 } 
+// export async function GET(){
+//     const headerOFNext =  await headers();
+//     console.log(headerOFNext.get('user-agent'));
+// }
